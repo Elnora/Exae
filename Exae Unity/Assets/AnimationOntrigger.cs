0bb94bfc50de20c8927343c392e68e25b1ff7bca
Caroline Fangel <carolinefangel@gmail.com>
2016-12-03 14:31:15 +0100

    no message

---
+++ Exae Unity/Assets/AnimationOntrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationOntrigger : MonoBehaviour 
+{
+	public Animation A;
+	private void OnTriggerEnter(Collider collision)
+	{
+		A.Play ();
+	}
+	private void OnTriggerExit(Collider collision)
+	{
+	}
+}
