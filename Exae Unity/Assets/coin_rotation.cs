703a8120a70cf6925cae21aa548bc56e1bc24cec
Mads Ny Larsen <mads@madeofdots.com>
2016-12-03 13:42:58 +0100

    no message

---
+++ Exae Unity/Assets/coin_rotation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coin_rotation : MonoBehaviour {
+
+	public Light lt;
+	public float duration = 6.0F;
+
+	// Use this for initialization
+	void Start () {
+		lt = GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (0, 0, 3, Space.Self);
+
+		float phi = Time.time / duration * 2 * Mathf.PI;
+		float amplitude = Mathf.Cos(phi) * 0.5F + 0.5F;
+		lt.intensity = amplitude;
+	}
+}
+
