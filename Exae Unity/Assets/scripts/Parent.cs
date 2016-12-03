36e2a8c2ff4370fc2ad94ff30b8423a1918666df
Mads Ny Larsen <mads@madeofdots.com>
2016-12-03 01:05:38 +0100

    no message

---
+++ Exae Unity/Assets/scripts/Parent.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+public class Parent : MonoBehaviour
+{
+	public Transform parent;
+	private void OnTriggerEnter(Collider collision)
+	{
+		collision.transform.SetParent(parent);
+	}
+	private void OnTriggerExit(Collider collision)
+	{
+		collision.transform.SetParent(null);
+	}
+}
